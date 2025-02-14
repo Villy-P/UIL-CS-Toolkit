0307b3e6b2aa24b3cda638773eb5389ef5557031
Villy-P <valeriuspetrini@gmail.com>
2025-02-14 08:13:45 -0600

    feat: enhance Button component with optional arrow icon and update homepage button text

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
-    let { text, href }: {
+    let { text, href, arrow = false }: {
         text: string;
         href: string;
+        arrow?: boolean;
     } = $props();
 </script>
 
-<a class="bg-black text-white px-5 py-2 rounded-lg hover:bg-gray-950 w-fit shadow" {href}>{text}</a>
+<a class="bg-[#2053c0] text-white px-5 py-2 rounded-lg hover:bg-[#1a56db] w-fit shadow flex items-center gap-1" {href}>
+    {text}
+    {#if arrow}
+        <img src="/icons/right-arrow.svg" class="w-4 h-4 ml-2 mt-0.5" alt="Right Arrow"/>
+    {/if}
+</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,12 @@
 </script>
 
 <div class="w-full h-screen flex items-center">
-    <div class="px-20 flex flex-col gap-4">
-        <h1 class="text-5xl font-extrabold">UIL Computer Science Toolkit</h1>
-        <p>An all-in-one toolkit for everything you need for the University Interscholastic League Computer Science Competition, from cheatsheets and guides about important topics, to questions directly from UIL tests to help you improve.</p>
+    <div class="px-10 flex flex-col gap-4">
+        <h1 class="text-6xl font-extrabold">UIL Computer Science Toolkit</h1>
+        <p class="text-md">An all-in-one toolkit for everything you need for the University Interscholastic League Computer Science Competition, from cheatsheets and guides about important topics, to questions directly from UIL tests to help you improve.</p>
         <div class="flex gap-3">
             <UnderlineButton text="Reference Sheet" href="/topics"/>
-            <Button text="Get Started" href="/topics"/>
+            <Button text="View Topics" href="/topics" arrow/>
         </div>
     </div>
 </div>
