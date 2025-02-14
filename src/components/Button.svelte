38e30a3526ec5d981bbb616d8459153586866f29
Villy-P <valeriuspetrini@gmail.com>
2025-02-13 19:29:53 -0600

    feat: add Button and UnderlineButton components with navigation links to homepage

---
+++ src/components/Button.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    let { text, href }: {
+        text: string;
+        href: string;
+    } = $props();
+</script>
+
+<a class="bg-black text-white px-5 py-2 rounded-lg hover:bg-gray-950 w-fit shadow" {href}>{text}</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
+<script>
+	import Button from "../components/Button.svelte";
+	import UnderlineButton from "../components/UnderlineButton.svelte";
+</script>
+
 <div class="w-full h-screen flex items-center">
     <div class="px-20 flex flex-col gap-4">
         <h1 class="text-5xl font-extrabold">UIL Computer Science Toolkit</h1>
         <p>An all-in-one toolkit for everything you need for the University Interscholastic League Computer Science Competition, from cheatsheets and guides about important topics, to questions directly from UIL tests to help you improve.</p>
+        <div class="flex gap-3">
+            <UnderlineButton text="Reference Sheet" href="/topics"/>
+            <Button text="Get Started" href="/topics"/>
+        </div>
     </div>
 </div>
