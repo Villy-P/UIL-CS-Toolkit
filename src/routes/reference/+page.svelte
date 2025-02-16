5571c76c296324e86720f214e3771a4ae255ee4e
Villy-P <valeriuspetrini@gmail.com>
2025-02-15 21:06:57 -0600

    feat: update Navbar links and enhance Reference Sheet description

--- src/routes/reference/+page.svelte
+++ src/routes/reference/+page.svelte
@@ -1,4 +1,4 @@
 <div class="px-10 flex flex-col gap-4 pt-32">
     <h1 class="text-6xl font-extrabold">Reference Sheet</h1>
-    <p class="text-md">This is the class reference sheet. You'll get one at every competition, and it contains useful info for what returns types and parameters are for certain function</p>
+    <p class="text-md">This is the class reference sheet. You'll get one at every competition, and it contains useful info for what returns types and parameters are for certain functions, classes, and interfaces. You can view it in a markdown format, or in the original PDF format.</p>
 </div>
---
+++ src/routes/reference/reference.md
@@ -0,0 +1,1 @@
+# Standard Classes and Interfaces
