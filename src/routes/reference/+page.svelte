dc40402ffdcbe0fd9713f34dc2fa764b949da443
Villy-P <valeriuspetrini@gmail.com>
2025-02-16 14:59:10 -0600

    style: adjust layout and spacing for Reference Sheet page

--- src/routes/reference/+page.svelte
+++ src/routes/reference/+page.svelte
@@ -1,7 +1,7 @@
-<div class="px-10 flex flex-col gap-4 pt-32">
+<div class="flex flex-col gap-4 pt-32 w-11/12 m-auto pb-2">
     <h1 class="text-6xl font-extrabold">Reference Sheet</h1>
     <p class="text-md">This is the class reference sheet. You'll get one at every competition, and it contains useful info for what returns types and parameters are for certain functions, classes, and interfaces. You can also <a href="./reference/class-reference-sheet.pdf" target="_blank" class="text-blue-400 hover:text-blue-500 underline">open it in a new tab.</a></p>
 </div>
-<div class="w-11/12 m-auto h-[calc(100vh-100px)] py-10">
+<div class="w-11/12 m-auto h-[calc(100vh-100px)] py-5">
     <embed src="./reference/class-reference-sheet.pdf" style="width: 100%;height: 100%;border: none;" title="Class Reference Sheet"/>
 </div>
