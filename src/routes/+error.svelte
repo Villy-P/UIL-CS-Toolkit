d07efeb5d4ed501162f90a1a135fbf79357dcdf9
Villy-P <valeriuspetrini@gmail.com>
2025-02-16 23:00:33 -0600

    feat: add custom 404 error page with user-friendly message

---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<div class="w-full min-h-[calc(100vh-120px)] flex items-center justify-center flex-col">
+    <h1 class="text-6xl font-extrabold">404</h1>
+    <p class="text-md">The page you are looking for does not exist.</p>
+    <a href="/">Go to Home Page</a>
+</div>
