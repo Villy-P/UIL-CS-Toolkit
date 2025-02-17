8d79c4f6de7a65a0780b3f0517c79555aee887f7
Villy-P <valeriuspetrini@gmail.com>
2025-02-16 23:21:25 -0600

    feat: integrate Analytics component into layout for enhanced tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
 	import '../app.css';
+	import Analytics from '$lib/Analytics.svelte';
 	import Footer from '../components/Footer.svelte';
 	import Navbar from '../components/Navbar.svelte';
 	let { children } = $props();
 </script>
+
+<Analytics/>
 
 <Navbar/>
 <div class="mt-14 min-h-[calc(100vh-120px)] wrapper">
