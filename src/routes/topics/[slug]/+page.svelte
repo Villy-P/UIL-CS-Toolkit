8d0bfea4b3af00e1fe3378b05fa7f36c04d79722
Villy-P <valeriuspetrini@gmail.com>
2025-02-16 20:20:33 -0600

    feat: add dynamic routing for topic cheatsheets on Topics page

---
+++ src/routes/topics/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from '$app/state';
+	import { goto } from "$app/navigation";
+	import { onMount } from 'svelte';
+
+    onMount(() => {
+        goto(`${page.params.slug}/cheatsheet`);
+    });
+</script>
