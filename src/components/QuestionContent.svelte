f764c6ea0b28ee3c21b8d1f6e7353fb710fb1ca1
Villy-P <valeriuspetrini@gmail.com>
2025-02-19 19:16:58 -0600

    feat: implement answer feedback and button click handling in QuestionContent

--- src/components/QuestionContent.svelte
+++ src/components/QuestionContent.svelte
@@ -6,15 +6,30 @@
         question: Question;
         index: number;
     } = $props();
+
+    let answered: boolean = $state(false);
+    let correct: boolean = $state(false);
+
+    function btnClick(index: number) {
+        if (answered) return;
+        answered = true;
+        correct = index === question.correct;
+    }
 </script>
 
 <h1>Question #{index + 1}</h1>
 <Markdown content={question.question}/>
 <div class="flex flex-col gap-1">
-    {#each question.answers as answer}
-        <button class="bg-[#151515] p-2 rounded-md w-96 cursor-pointer hover:bg-[#1f1f1f] my-0.5">
+    {#each question.answers as answer, index}
+        <button class="bg-[#151515] p-2 rounded-md w-96 cursor-pointer hover:bg-[#1f1f1f] my-0.5" onclick={() => btnClick(index)}>
             <Markdown content={answer}/>
         </button>
     {/each}
 </div>
+{#if answered}
+    <div class="w-96 {correct ? 'bg-green-500' : 'bg-red-500'} p-2 rounded-md text-white">
+        <Markdown content={correct ? "Correct!" : "Incorrect!"}/>
+        <Markdown content={question.explanation}/>
+    </div>
+{/if}
 <hr class="my-4"/>
