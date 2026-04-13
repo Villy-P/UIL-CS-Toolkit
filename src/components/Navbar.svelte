c790723f3d46c622ddd75b07ba8cc71d8a069500
Valerius Petrini <valeriuspetrini@gmail.com>
2026-04-13 18:54:42 +0000

    feat: integrate @valerius_petrini/corekit-ui for enhanced UI components

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,18 +1,14 @@
-<nav class="navbar bg-black fixed top-0 w-full border-b-slate-500 h-14 flex text-white z-[60]">
-    <a class="h-full ml-3 flex justify-center items-center cursor-pointer navitem" href="/">UIL Computer Science Toolkit</a>
-    <div class="flex ml-auto items-center">
-        <a class="h-full flex items-center px-5 hover:bg-gray-950 navitem" href="/topics">Topics</a>
-        <a class="h-full flex items-center px-5 hover:bg-gray-950 navitem" href="/resources">Resources</a>
-        <a class="h-full flex items-center px-5 hover:bg-gray-950 navitem" href="/reference">Reference Sheet</a>
-        <a class="h-full flex items-center px-5 hover:bg-gray-950 navitem" href="https://github.com/Villy-P/UIL-CS-Toolkit" target="_blank">
-            <img class="h-8" src="/social/github-mark-white.svg" alt="Github Page"/>
-        </a>
-    </div>
-</nav>
+<script>
+	import { Navbar, NavbarElement, NavbarSeparator, Button } from "@valerius_petrini/corekit-ui";
+</script>
 
-<style>
-    .navitem {
-        color: white;
-        text-decoration: none;
-    }
-</style>
+<Navbar>
+    <NavbarElement href="/">Home</NavbarElement>
+    <NavbarSeparator/>
+    <NavbarElement href="/topics">Topics</NavbarElement>
+    <NavbarElement href="/resources">Resources</NavbarElement>
+    <NavbarElement href="/reference">Reference</NavbarElement>
+    <Button href="https://github.com/Villy-P/UIL-CS-Toolkit" external>
+        <img class="h-6" src="/social/github-mark-white.svg" alt="Github Page"/>
+    </Button>
+</Navbar>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,48 @@
 <script>
-	import Button from "../components/Button.svelte";
+	import { Text, Button, Card } from "@valerius_petrini/corekit-ui";
 	import SEO from "../components/SEO.svelte";
-	import UnderlineButton from "../components/UnderlineButton.svelte";
 </script>
 
 <div class="w-full h-[calc(100vh-56px)] flex items-center border-b-2 border-gray-400">
     <div class="px-10 flex flex-col gap-4">
-        <h1 class="!text-6xl font-extrabold">UIL Computer Science Toolkit</h1>
-        <p class="text-md">An all-in-one toolkit for everything you need for the University Interscholastic League Computer Science Competition, from cheatsheets and guides about important topics, to questions directly from UIL tests to help you improve.</p>
+        <Text tag="h1">UIL Computer Science Toolkit</Text>
+        <Text>An all-in-one toolkit for everything you need for the University Interscholastic League Computer Science Competition, from cheatsheets and guides about important topics, to questions directly from UIL tests to help you improve.</Text>
         <div class="flex gap-3">
-            <UnderlineButton text="Reference Sheet" href="/reference"/>
-            <Button text="View Topics" href="/topics" arrow/>
+            <Button variant="outline" color="white" href="/reference">Reference Sheet</Button>
+            <Button href="/topics" color="blue">
+                View Topics
+                <img src="/icons/right-arrow.svg" class="w-4 h-4 ml-2 mt-0.5" alt="Right Arrow"/>
+            </Button>
         </div>
     </div>
 </div>
-<div class="w-full flex items-center justify-center flex-wrap gap-5 my-20">
-    <div class="w-80 h-[380px] shadow shadow-black p-5 m-5 rounded-lg bg-[#161616] hover:bg-[#1c1c1c]">
-        <h2 class="text-2xl font-bold text-center pb-6">Resources</h2>
-        <p>View Resources relating to Java topics. This can range from other websites to practice Java programming, to Java fundamentals, to focusing on certain subtopics like strings or bases. Practice programming with LeetCode or study Java fundamentals with W3Schools</p>
-        <Button text="View Resources" href="/resources" arrow className="m-auto mt-3"/>
-    </div>
-    <div class="w-80 h-[380px] shadow shadow-black p-5 m-5 rounded-lg bg-[#161616] hover:bg-[#1c1c1c]">
-        <h2 class="text-2xl font-bold text-center pb-6">Reference Sheet</h2>
-        <p>Check out reference material that is given to you on the day of the UIL competition.</p>
-        <Button text="Reference Sheet" href="/reference" arrow className="m-auto mt-3"/>
-    </div>
-    <div class="w-80 h-[380px] shadow shadow-black p-5 m-5 rounded-lg bg-[#161616] hover:bg-[#1c1c1c]">
-        <h2 class="text-2xl font-bold text-center pb-6">Topics</h2>
-        <p>View different topics to help you get better, from strings, arrays, bases, Big O, etc. Quickly go over important topics using cheatsheets and view long form resources for learning more about topics.</p>
-        <Button text="View Topics" href="/topics" arrow className="m-auto mt-3"/>
-    </div>
+<div class="w-full flex items-center justify-center flex-wrap gap-5 my-20 px-5">
+    <Card class="flex flex-col gap-5 h-72">
+        <Text tag="h3">Resources</Text>
+        <Text>View Resources relating to Java topics. This can range from other websites to practice Java programming, to Java fundamentals, to focusing on certain subtopics like strings or bases. Practice programming with LeetCode or study Java fundamentals with W3Schools</Text>
+        <Button href="/resources" color="blue" class="mt-auto">
+            View Resources
+            <img src="/icons/right-arrow.svg" class="w-4 h-4 ml-2 mt-0.5" alt="Right Arrow"/>
+        </Button>
+    </Card>
+
+    <Card class="flex flex-col gap-5 h-72">
+        <Text tag="h3">Reference Sheet</Text>
+        <Text>Check out reference material that is given to you on the day of the UIL competition.</Text>
+        <Button href="/reference" color="blue" class="mt-auto">
+            View Reference Sheet
+            <img src="/icons/right-arrow.svg" class="w-4 h-4 ml-2 mt-0.5" alt="Right Arrow"/>
+        </Button>
+    </Card>
+
+    <Card class="flex flex-col gap-5 h-72">
+        <Text tag="h3">Topics</Text>
+        <Text>View different topics to help you get better, from strings, arrays, bases, Big O, etc. Quickly go over important topics using cheatsheets and view long form resources for learning more about topics.</Text>
+        <Button href="/topics" color="blue" class="mt-auto">
+            View Topics
+            <img src="/icons/right-arrow.svg" class="w-4 h-4 ml-2 mt-0.5" alt="Right Arrow"/>
+        </Button>
+    </Card>
 </div>
 
 <SEO title="UIL CS Toolkit" description="An all-in-one toolkit for everything you need for the University Interscholastic League Computer Science Competition, from cheatsheets and guides about important topics, to questions directly from UIL tests to help you improve."/>
